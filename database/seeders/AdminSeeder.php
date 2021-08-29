a58e9de4d1e9eb37159fc22a3bd183d0a0ab8280
sizuwanoadi <adi15siswanto@gmail.com>
2021-08-29 16:31:38 +0700

    delete windows logs

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::firstOrCreate([
+            'name' => 'Developer',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('123456')
+        ]);
+    }
+}
